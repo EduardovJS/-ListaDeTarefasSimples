7764b0bc64a0404b9c9fdb572fcbac3fdf6bd0b0
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-10-10 22:04:46 -0300

    Conexão com o banco de dados e models

---
+++ ListaDeTarefasSimples/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ListaDeTarefasSimples.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeTarefasSimples.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Tarefas> Tarefas { get; set; }
+
+    }
+}
---
+++ ListaDeTarefasSimples/Models/Tarefas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ListaDeTarefasSimples.Models
+{
+    [Table("Tarefas")]
+    public class Tarefas
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(200, ErrorMessage = "O tamanho máximo é de 200 caracteres")]
+        [Required(ErrorMessage = "Informe a tarefa")]
+        [Display(Name = "Tarefa")]
+        public string Tarefa { get; set; }
+        public DateTime Data { get; set; }
+        public bool Status { get; set; }
+    }
+
+
+}
+
